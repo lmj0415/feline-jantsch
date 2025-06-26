9d1f0e9cccf4f19634b6a8847ae4bc269cdea6ab
grandolf14 <standartnoop@web.de>
2025-06-26 14:56:54 +0200

    v14:56

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 let slideIndex = 1;
 
 /* to be replaced by server delivery */
-let texts = ["London1", "London2", "London3", "London4","London5"]
-const links = ["./src/images/img-1.jpg","./src/images/img-1.jpg","./src/images/img-2.jpg","./src/images/img-3.jpg","./src/images/img-3.jpg"];
+let texts = ["London1", "London2", "London3", "London4","London5","London1", "London2", "London3", "London4","London5","London1", "London2", "London3", "London4","London5","London1", "London2", "London3", "London4","London5","London1", "London2", "London3", "London4","London5","London1", "London2", "London3", "London4","London5"]
+const links = ["./src/images/img-1.jpg","./src/images/img-1.jpg","./src/images/img-2.jpg","./src/images/img-3.jpg","./src/images/img-3.jpg","./src/images/img-1.jpg","./src/images/img-1.jpg","./src/images/img-2.jpg","./src/images/img-3.jpg","./src/images/img-3.jpg","./src/images/img-1.jpg","./src/images/img-1.jpg","./src/images/img-2.jpg","./src/images/img-3.jpg","./src/images/img-3.jpg","./src/images/img-1.jpg","./src/images/img-1.jpg","./src/images/img-2.jpg","./src/images/img-3.jpg","./src/images/img-3.jpg","./src/images/img-1.jpg","./src/images/img-1.jpg","./src/images/img-2.jpg","./src/images/img-3.jpg","./src/images/img-3.jpg","./src/images/img-1.jpg","./src/images/img-1.jpg","./src/images/img-2.jpg","./src/images/img-3.jpg","./src/images/img-3.jpg"];
 
 
 function start(){
@@ -11,7 +11,7 @@ function start(){
       document.getElementById('imgSlider').innerHTML += `
       <img src="${link}" id="slide-${counter}">`
       document.getElementById('dot2').innerHTML += `
-          <span class="dot" onclick="currentSlide(${counter})"></span>`
+          <img class="dot" src="${link}" onclick="currentSlide(${counter})" id='sel-${counter}'>`
       counter++;};
     currentSlide(slideIndex);
 }
@@ -19,6 +19,7 @@ function start(){
 // Next/previous controls
 function plusSlides(n) {
   showSlides(slideIndex += n);
+  console.log(slideIndex);
 }
 
 // Thumbnail image controls
@@ -28,7 +29,7 @@ function currentSlide(n) {
 
 function showSlides(n){
 if (n > links.length) {slideIndex = 1};
-if (n < 1) {slideIndex = slides.length};
+if (n < 1) {slideIndex = links.length};
 document.location=`#slide-${slideIndex}`;
 document.getElementById('text').innerHTML = texts[slideIndex-1];
 
